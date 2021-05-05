18ad433e71ab108d3c0174c334e63baa1b80aaa3
tomoko.kuga <kuga.tomoko0423@gmail.com>
2021-05-05 10:18:32 +0000

    tasklist_login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,20 @@
 class TasksController < ApplicationController
+    before_action :require_user_logged_in
     before_action :set_task, only: [:show, :edit, :update, :destroy]
+    
     def index
-        @tasks = Task.all
+      @tasks = current_user.tasks.all
     end
     
     def show
     end
 
     def new
-        @task = Task.new
+        @task = current_user.tasks.build
     end
     
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
     
     if @task.save
         flash[:success] = 'タスクが正常に投稿されました'
@@ -27,7 +29,6 @@ class TasksController < ApplicationController
     end
     
     def update
-
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
